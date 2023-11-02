7bdbabe7238e8e7ef8f30f7a236b62eb1ce0d07d
itsmylifestyle <hahaii00@mail.ru>
2023-11-02 15:33:25 +0600

    Initial Commit

---
+++ MLImageRecognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MLImageRecognition
+//
+//  Created by Айбек on 02.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
